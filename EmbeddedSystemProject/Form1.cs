3c5f659abf28f383a1113afd9b48d06f2be8bce6
Henkahau <henri.haukipuro@kotinet.com>
2017-11-13 11:15:22 +0200

    lisätty tietokantaan yhdistäminen

--- EmbeddedSystemProject/Form1.cs
+++ EmbeddedSystemProject/Form1.cs
@@ -7,16 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Threading;
 
 namespace EmbeddedSystemProject
 {
     public partial class Form1 : Form
     {
-        float dataFloat;
-        string dataString;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //connect to MySql database
+            string connectionString = "server='192.168.137.76'; database=weatherLog; user=user; password = pass;";
+
+            MySqlConnection myConnection = new MySqlConnection(connectionString);
+
+            myConnection.Open();
+
+            if (myConnection.State != ConnectionState.Open)
+                MessageBox.Show("Cannot connect to database");
+
+            MySqlDataReader dataReader = null;
+
+            MySqlCommand myCommand = new MySqlCommand("SELECT * FROM dataLog ORDER BY id DESC LIMIT 1", myConnection);
+
+            while(myConnection.State == ConnectionState.Open)
+            {
+                //read data from db to console
+                dataReader = myCommand.ExecuteReader();
+                dataReader.Read();
+                Console.WriteLine(dataReader.GetFloat(2));
+                dataReader.Close();
+                Thread.Sleep(1000);
+
+            }
         }
     }
 }
