91ed40c5f6768e4a2f75fe612c1e67c300ce5a15
Henkahau <henri.haukipuro@kotinet.com>
2017-11-13 13:21:13 +0200

    versio v0.02 lisätty timer

--- EmbeddedSystemProject/Form1.cs
+++ EmbeddedSystemProject/Form1.cs
@@ -23,7 +23,7 @@ namespace EmbeddedSystemProject
         public Form1()
         {
             InitializeComponent();
-            
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
