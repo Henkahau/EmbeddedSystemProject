a06ccec172c9f9ca1fb3086f85be0279e9ba645d
Henkahau <henri.haukipuro@kotinet.com>
2017-11-13 13:17:26 +0200

    koodiin lisätty timer, jotta saadaan data luettua sekunnin välein tietokannasta

--- EmbeddedSystemProject/Form1.cs
+++ EmbeddedSystemProject/Form1.cs
@@ -9,15 +9,21 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using System.Threading;
+using System.Timers;
 
 namespace EmbeddedSystemProject
 {
     public partial class Form1 : Form
     {
-        
+        private string dataStr;
+        private System.Timers.Timer timer;
+        MySqlConnection myConnection;
+        MySqlCommand myCommand;
+
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,27 +31,41 @@ namespace EmbeddedSystemProject
             //connect to MySql-database
             string connectionString = "server='192.168.137.76'; database=weatherLog; user=user; password = pass;";
 
-            MySqlConnection myConnection = new MySqlConnection(connectionString);
+            myConnection = new MySqlConnection(connectionString);
 
             myConnection.Open();
 
             if (myConnection.State != ConnectionState.Open)
                 MessageBox.Show("Cannot connect to database");
+            else
+                Console.WriteLine("Connection ok");
+            
+            //tehdään timer 
+            timer = new System.Timers.Timer(1000);
+            timer.Start();
+            timer.Elapsed += readDataFromDb;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxData.Text = dataStr;
+        }
+
+        private void readDataFromDb(object source, ElapsedEventArgs e)
+        {
+            //tämä suoritetaan kun timer on päässyt loppuun
 
             MySqlDataReader dataReader = null;
+            myCommand = new MySqlCommand("SELECT * FROM dataLog ORDER BY id DESC LIMIT 1", myConnection);
 
-            MySqlCommand myCommand = new MySqlCommand("SELECT * FROM dataLog ORDER BY id DESC LIMIT 1", myConnection);
-
-            while(myConnection.State == ConnectionState.Open)
-            {
-                //read data from db to console
-                dataReader = myCommand.ExecuteReader();
-                dataReader.Read();
-                Console.WriteLine(dataReader.GetFloat(2));
-                dataReader.Close();
-                Thread.Sleep(1000);
-
-            }
+            //read data from db to console
+            dataReader = myCommand.ExecuteReader();
+            dataReader.Read();
+            dataStr = dataReader.GetFloat(2).ToString();
+            Console.WriteLine(dataReader.GetFloat(2));
+            //textBoxData.Text = dataStr;
+            dataReader.Close();
         }
     }
 }
